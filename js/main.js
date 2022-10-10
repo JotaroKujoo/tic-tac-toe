8d7ea755f43e98d8ca8042df212a8cbe9ba0d77f
Jose Rodriguez <rdrgz.jose123@gmail.com>
2022-10-11 00:19:54 +0200

    Actualizando pagina players y pagina game

--- js/main.js
+++ js/main.js
@@ -1,31 +1,59 @@
 
 
 
+//OBTENEMOS LOS USUARIOS
 
 
 const getUser = () => {
     let inputValue = document.getElementById("user1").value;
-    let jsonForm = {
-        name: inputValue
-    }
-
-    const newUser = JSON.stringify(jsonForm)
-  }
+    return inputValue
+}
   
   
-  const getCPU = () => {
-    let inputValue = document.getElementById("CPU").value;
-    return inputValue 
-  }
+const getUser2 = () => {
+  let inputValue = document.getElementById("user2").value;
+  return inputValue 
+}
   
 
 
+const saveUsers = (user1,user2) => {
+  sessionStorage.setItem("first",user1)
+  sessionStorage.setItem("second",user2)
+}
 
-  const Mostrar = (usuario1,usuario2) => {
-    JSON.parse(usuario1,usuario2)
-  }
 
 
+const buttonSave = document.getElementById("saveButton")
+
+buttonSave.addEventListener("click",saveUsers(getUser(),getUser2()))
+  
+
+
+
+
+
+let opciones = Array.from(document.getElementsByClassName("radio"))
+
+opciones.map((opcion)=>{
+    opcion.addEventListener("click",()=>{
+      switch ( opcion.value){
+        case "PlayerVsCPU":
+            document.getElementById("firstPlayer").innerHTML = "Name Player"
+            document.getElementById("secondPlayer").innerHTML = "Name CPU"
+          break;
+        case "CPUvsPlayer":
+          document.getElementById("firstPlayer").innerHTML = "Name CPU"
+          document.getElementById("secondPlayer").innerHTML = "Name Player"
+          break;
+        case "PlayerVsPlayer":
+          document.getElementById("firstPlayer").innerHTML = "Name Player"
+          document.getElementById("secondPlayer").innerHTML = "Name Player 2"
+          break;
+        
+      }
+    })
+})
 
 //   function myKeyUpFunction() {
 
